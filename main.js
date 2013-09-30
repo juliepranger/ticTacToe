a9214f256ea08aed8ffb4712134c6987453b6331
Julie Pranger <juliepranger@gmail.com>
2013-09-29 20:55:48 -0700

    overrode auto X and O filling, added popup to select an open square

--- main.js
+++ main.js
@@ -3,27 +3,47 @@
 
 
 // Will User be X or O? 
+
 /*function start(){
 	alert("js works")
-} */
+} 
+*/
 
 var myTurn = 0
 var mySymbols = ""
 
 function Click() {
 
-	if(myTurn == 0)
-		mySymbols =  "X";
-	else 
-		mySymbols = "O";
+	if(event.target.innerHTML != "")
+		alert("Choose an empty box!");
+
+	else {if(myTurn == 0)
+			mySymbols =  "X";
+		else 
+			mySymbols = "O";
 	
 	event.target.innerHTML = mySymbols	
 	
 	if(myTurn == 0)
-		myTurn = 1
+		myTurn = 1;
 	else 
 		myTurn = 0;
-	} 
+	}
+} 
+// choose an empty box if the clicked box has already been filled!
+
+function clickBox() {
+
+	if(event.target.innerHTML != "")
+		alert("Choose an empty box!");
+	else 
+		mySymbols;
+
+}
+
+function startGame() {
+	document.getElementById('begingame').style.top = "-10000px";
+}
 
 // How do I determine who the winner is? How can I ensure Xs and Os are not overridden?
 
